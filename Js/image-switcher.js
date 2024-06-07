88c1e4de37a42f9d78811318b5c449ec94ddb910
QLMio <153973485+QLMio@users.noreply.github.com>
2024-06-07 16:33:30 +0800

    更新 image-switcher.js

--- Js/image-switcher.js
+++ Js/image-switcher.js
@@ -1,28 +1,36 @@
-<script src="//geoip-js.com/js/apis/geoip2/v2.1/geoip2.js" type="text/javascript"></script>
-<script>
-  document.addEventListener('DOMContentLoaded', function() {
-    geoip2.country(function(geoipResponse) {
-      var images = document.getElementsByTagName('img');
-      for (var i = 0; i < images.length; i++) {
-        var img = images[i];
-        var src = img.getAttribute('src');
-        if (src.includes('github.com') || src.includes('gitee.com')) {
-          if (geoipResponse.country.iso_code === 'CN') {
-            img.src = src.replace('github.com', 'gitee.com');
-          } else {
-            img.src = src.replace('gitee.com', 'github.com');
-          }
-          img.onerror = function() {
-            if (this.src.includes('gitee.com')) {
-              this.src = this.src.replace('gitee.com', 'github.com');
-            } else if (this.src.includes('github.com')) {
-              this.src = this.src.replace('github.com', 'gitee.com');
-            }
-          };
-        }
-      }
-    }, function(error) {
-      console.log('Error occurred: ' + error);
-    });
+// 在Hexo主题的 `source/js` 目录下创建一个新的JS文件，例如 `image-loader.js`
+document.addEventListener('DOMContentLoaded', function() {
+  // 获取所有图片元素
+  const images = document.querySelectorAll('img');
+  images.forEach(img => {
+    // 检查图片URL是否为GitHub或Gitee的图床
+    if (/github\.com/.test(img.src) || /gitee\.com/.test(img.src)) {
+      // 创建一个新的Image对象用于测试图片加载
+      const testImage = new Image();
+      testImage.onload = function() {
+        // 如果图片加载成功，不做任何操作
+      };
+      testImage.onerror = function() {
+        // 如果图片加载失败，尝试切换图床
+        switchImageSource(img);
+      };
+      // 尝试加载图片
+      testImage.src = img.src;
+    }
   });
-</script>
+
+  function switchImageSource(img) {
+    // 根据用户的地理位置切换图片源
+    const isChina = (new Date().getTimezoneOffset() / -60) === 8; // 中国标准时间为GMT+8
+    let newSrc;
+    if (isChina) {
+      // 如果用户在中国，将GitHub图床链接替换为Gitee
+      newSrc = img.src.replace('github.com', 'gitee.com');
+    } else {
+      // 如果用户不在中国，将Gitee图床链接替换为GitHub
+      newSrc = img.src.replace('gitee.com', 'github.com');
+    }
+    // 更新图片源并重新加载
+    img.src = newSrc;
+  }
+});
